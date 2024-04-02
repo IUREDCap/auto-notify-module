d43b30fc965adfe97695ef27bb7e68dca1fca293
Jim Mullen <mullen2@iu.edu>
2024-04-02 16:34:17 -0400

    Changed capitalization of null to make it consistent with other instances

--- resources/queryBuilder.js
+++ resources/queryBuilder.js
@@ -255,7 +255,7 @@ AutoNotifyModule.getCondition = function(variable = null, operator = null, value
         }
     }
     else if (data.valueType === "null") {
-        html += '<input type="text" size="4" value="NULL" readonly></input>';
+        html += '<input type="text" size="4" value="null" readonly></input>';
     }
     else if (data.valueType === "select") {
         html += '<select>' + "\n";
